beefe6cffbc69ad770353b44e6c4dcb4f324bb57
francobustos23 <bustosfranco071@gmail.com>
2024-10-17 12:16:58 -0300

    routes

---
+++ src/routes/client.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { ClientController } from "../controllers/client.controllers";
+import { ClientService } from "../services/client.service";
+import { ClientPostgresRepository } from "../repositories/client.postgres.repository";
+
+const router: Router = Router();
+
+const clientRepository = new ClientPostgresRepository();
+const clientService = new ClientService(clientRepository);
+const clientController = new ClientController(clientService);
+
+router.post('/clients', 
+    (req,res) => {
+    clientController.create(req,res);       
+});
+router.get('/clients/:id',
+    (req,res) => {
+    clientController.findById(req,res);
+});
+router.put('/clients/:id',
+    (req,res) => {
+    clientController.update(req,res);
+});
+router.delete('/clients/:id',
+    (req,res) => {
+    clientController.delete(req,res);
+});
+
+export default router;
