d0ee5f124d9b2571aa0e1ec5337e676343905149
francobustos23 <bustosfranco071@gmail.com>
2024-10-17 12:16:10 -0300

    interfaces

---
+++ src/interfaces/client.repository.interface.ts
@@ -0,0 +1,16 @@
+import { IClient } from "../models/client.model";
+
+export interface IClientRepository {
+    
+    create(client: IClient):
+        Promise<void>;
+
+    findById(id: string):
+        Promise<IClient | null>;
+
+    update(id: string, client: IClient):
+        Promise<void>;
+
+    delete(id: string):
+        Promise<void>;
+}
---
+++ src/interfaces/vehicle.repository.interface.ts
@@ -0,0 +1,16 @@
+import { Ivehicle } from "../models/vehicle.model";
+
+export interface IVehicleRepository {
+
+    create(vehicle: Ivehicle):
+        Promise<void>;
+
+    findById(id: string):
+        Promise<Ivehicle | null>;
+
+    update(id: string, vehicle: Ivehicle):
+        Promise<void>
+
+    delete(id: string):
+        Promise<void>;
+}
