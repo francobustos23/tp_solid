e921a903181a145414f2550ec42227ddaeaa34fb
francobustos23 <bustosfranco071@gmail.com>
2024-10-17 12:15:49 -0300

    environments

---
+++ src/environments/environments.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import * as env from 'env-var'
+import { Dialect } from 'sequelize'
+
+export const envsSQL = {
+    PORT: env.get('PORT').required().asPortNumber(),
+    DB_HOST: env.get('DB_HOST').required().asString(),
+    DB_PORT: env.get('DB_PORT').required().asPortNumber(),
+    DB_USER: env.get('DB_USER').required().asString(),
+    DB_PASSWORD: env.get('DB_PASSWORD').required().asString(),
+    DB_NAME: env.get('DB_NAME').required().asString(),
+    DB_DIALECT: process.env.DB_DIALECT as Dialect
+}
+
+export const envsMongo = {
+    PORT: env.get('PORT').required().asPortNumber(),
+    URI:  env.get('URI').required().asString()
+}
