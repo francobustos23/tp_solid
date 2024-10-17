8a62f73b1f0b9ff0e31ad94eee34f9ace9c074e0
francobustos23 <bustosfranco071@gmail.com>
2024-10-17 16:02:24 -0300

    vehicle postgres repository

--- src/repositories/vehicle.postgres.repository.ts
+++ src/repositories/vehicle.postgres.repository.ts
@@ -7,19 +7,25 @@ export class VehiclePostgresRepository implements IVehicleRepository {
     private vehicles: Ivehicle[] = [];
 
     async create(vehicle: Ivehicle): Promise<void> {
-        this.vehicles.push(vehicle);
+        this.vehicles
+        .push(vehicle);
         console.log('Vehicle created in Postgres: ', vehicle);
     }
 
     async findById(id: string): Promise<Ivehicle | null> {
-        const vehicle = this.vehicles.find(search_vehicle => search_vehicle.id === id);
+        const vehicle = 
+        this.vehicles
+        .find(search_vehicle => search_vehicle.id === id);
         console.log('Vehicle found in Postgres: ', vehicle);
 
         return vehicle || null
     }
 
     async update(id: string, vehicle: Ivehicle): Promise<void> {
-        const index = this.vehicles.findIndex(search_vehicle => search_vehicle.id === id)
+        const index = 
+        this.vehicles
+        .findIndex(search_vehicle => search_vehicle.id === id)
+        
         if(index !== -1) {
             this.vehicles[index] = vehicle;
             console.log('Vehicle update in Postgres', vehicle)
@@ -28,7 +34,9 @@ export class VehiclePostgresRepository implements IVehicleRepository {
     }
 
     async delete(id: string): Promise<void> {
-        this.vehicles = this.vehicles.filter(vehicle => vehicle.id !== id);
+        this.vehicles = 
+        this.vehicles
+        .filter(vehicle => vehicle.id !== id);
         console.log('Vehicle deleted in Postgres', id) 
     }
 
