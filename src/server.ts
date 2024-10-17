63f5c338a689aee2bbaece94f8a71d99ad6dfa6e
francobustos23 <bustosfranco071@gmail.com>
2024-10-17 12:18:25 -0300

    file server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import express, { Application } from 'express'
+import clientRoutes from './routes/client.routes'
 
 export class Server {
     private app: Application;
@@ -10,6 +11,20 @@ export class Server {
     constructor() {
         this.app = express();
         this.port = 3000;
+
+        this.middleweares();
+        this.routes();
+    }
+
+    middleweares() {
+        this.app.use(cors());
+        this.app.use(helmet());
+        this.app.use(morgan('dev'));
+        this.app.use(express.json());
+    }
+
+    routes() {
+        this.app.use('/api', clientRoutes);
     }
 
     public listen(): void {
